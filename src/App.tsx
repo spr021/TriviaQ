db2f110503c28013072c5c1f9f682592b2235550
Saber <saber.pourrahimi.1999@gmail.com>
2024-06-12 16:44:41 +0200

    Add firebase And set new users to db

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,32 @@ import AppBar from "./components/appbar/AppBar"
 import useTimer from "./hooks/useTimer/useTimer"
 import Timer from "./components/timer/Timer"
 import SecondsCounter from "./components/secound-counter/SecoundCounter"
+import { useEffect } from "react"
+import { collection, addDoc, getDocs, orderBy, query } from "firebase/firestore"
+import WebApp from "@twa-dev/sdk"
+import { db } from "./Firebase"
 
 function App() {
   const { timeLeft, screenMode } = useTimer()
+
+  useEffect(() => {
+    const q = query(collection(db, "users"), orderBy("id", "asc"))
+    let isUserExist = false
+    getDocs(q).then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        if (doc.data().id === WebApp.initDataUnsafe.user?.id) {
+          isUserExist = true
+        }
+      })
+      if (!isUserExist) {
+        addDoc(collection(db, "users"), {
+          id: WebApp.initDataUnsafe.user?.id,
+          username: WebApp.initDataUnsafe.user?.username,
+          heart: 3,
+        })
+      }
+    })
+  }, [])
 
   return (
     <>
